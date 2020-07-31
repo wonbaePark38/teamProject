ad7ba97c2fb1987424d9e2d96273b37a02fead06
DESKTOP-3VNT1VB\kjh <berlet52@gmail.com>
2020-07-31 10:46:34 +0900

    no message

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -33,25 +33,3 @@ function searchPress(){
   console.log('클릭');
 }
     
-google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['요청', 5],
-          ['진행', 1],
-          ['피드백', 3],
-          ['완료', 9],
-          ['보류', 1]
-        ]);
-
-        var options = {
-          title: '업무 리포트'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-}
