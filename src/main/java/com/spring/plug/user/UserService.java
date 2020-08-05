8c8e775eae1c2095f4f6e53de424e1868039206e
wonbae <deux38@naver.com>
2020-08-05 09:17:48 +0900

    Revert "소셜로그인 암호화전"
    This reverts commit 65854fec4d4cca8c48078785f8e2f38a14d4f2db.

--- src/main/java/com/spring/plug/user/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.spring.plug.user;
-
-public interface UserService {
-	public UserVO getUser(UserVO vo);
-}
--- src/main/java/com/spring/plug/user/UserVO.java
+++ src/main/java/com/spring/plug/user/UserVO.java
@@ -1,38 +1,9 @@
-package com.spring.plug.user;
-
+package com.plug.web.user;
 
 public class UserVO {
-	
-	private String password;
-	private String name;
+
+	private String userId;
+	private String userPw;
 	private String email;
-	
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	public String toString() {
-		return "UserVO [password=" + password + ", name=" + name + ", email=" + email + "]";
-	}
-	
-	
-	
 	
 }
