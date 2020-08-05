96764d4dfdcf74d6a8b20aaaa21f2f6c9768a901
wonbae <deux38@naver.com>
2020-08-05 15:51:19 +0900

    userdao 수정

--- src/main/java/com/spring/plug/login/dao/UserDAO.java
+++ src/main/java/com/spring/plug/login/dao/UserDAO.java
@@ -15,11 +15,19 @@ public class UserDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 	
-	//private final String USER_GET = "select * from wonbaetest where email= ? and password= ?";
-	
 	public UserVO getUser(UserVO vo) {
 		System.out.println("--->MyBatis로  login 기능 처리");
 		return (UserVO) sqlSessionTemplate.selectOne("UserDAO.getUser",vo);
 	}
+	
+	public UserVO getSocialUser(UserVO vo) {
+		System.out.println("-->MyBatis로 소셜 login 기능 처리");
+		return (UserVO) sqlSessionTemplate.selectOne("UserDAO.getSocialUser",vo);
+	}
+	
+	/*public String getUserEmail(UserVO vo) {
+		System.out.println("-->MyBatis로 소셜 login 기능 처리");
+		return (UserVO) sqlSessionTemplate.selectOne("UserDAO.getSocialUser",vo);
+	}*/
 }
 
